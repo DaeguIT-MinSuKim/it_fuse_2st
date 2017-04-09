a8fceb106c875cee92af27207acae4e53b388c39
Administrator <Administrator@59.25.192.30>
2017-04-10 05:44:18 +0900

    데이커 피커가 문제다

--- src/main/java/kr/or/dgit/book_project/ui/view/BookReturnView.java
+++ src/main/java/kr/or/dgit/book_project/ui/view/BookReturnView.java
@@ -1,6 +1,5 @@
 package kr.or.dgit.book_project.ui.view;
 
-import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -10,6 +9,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
@@ -17,12 +21,12 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import kr.or.dgit.book_project.dto.PaymentIO;
-import kr.or.dgit.book_project.ui.common.AbsViewPanel;
+import kr.or.dgit.book_project.service.PaymentIOService;
 import kr.or.dgit.book_project.ui.component.BookInfoBasic;
 import kr.or.dgit.book_project.ui.component.BookLendMemberDetailDate;
 import kr.or.dgit.book_project.ui.table.PaymentIoTable;
 
-public class BookReturnView extends JPanel {
+public class BookReturnView extends JPanel implements MouseListener {
 
 	private BookInfoBasic pBookinfo;
 	private JPanel pMemberinfo;
@@ -41,6 +45,7 @@ public class BookReturnView extends JPanel {
 		pContent.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		pBookinfo = new BookInfoBasic();
+		pBookinfo.getpBCode().getTfBCode().addMouseListener(this);
 		pContent.add(pBookinfo);
 		
 		pMemberinfo = new JPanel();
@@ -93,6 +98,12 @@ public class BookReturnView extends JPanel {
 		add(pTabel);
 	}
 
+	public void mousePressed(MouseEvent e) {
+		if (e.getSource() == pBookinfo.getpBCode().getTfBCode()) {
+			pBookinfoPBCodeTfBCodeMousePressed(e);
+		}
+	}
+	
 	public PaymentIoTable getpTabel() {
 		return pTabel;
 	}
@@ -116,7 +127,63 @@ public class BookReturnView extends JPanel {
 		}
 	}
 	protected void actionPerformedBtnLend(ActionEvent arg0) {
-		PaymentIO paymentio = pTabel.getSelectedObject();
-		JOptionPane.showMessageDialog(null, pMemberDetail.returnMsg());
+		// 반납 버튼 프로시저..이것만되면 슈바 끝인가...
+		/*PaymentIO paymentio = pTabel.getSelectedObject();
+		JOptionPane.showMessageDialog(null, pMemberDetail.returnMsg());*/
+		
+		PaymentIO bookinfo = pBookinfo.getObjectP();
+		PaymentIO memberinfo = pMemberDetail.getObject();
+		
+		Map<String, Object> param = new HashMap<>();
+		param.put("b_code", bookinfo.getBookInfo().getbCode());
+		param.put("b_sub_code", bookinfo.getBookInfo().getbSubCode());
+		param.put("m_code", memberinfo.getMemberInfo().getmCode());
+		// 여기서 막힘
+		/*SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		Date date = sdf.parse(param.put("return_date", memberinfo.getReturnDate()));*/
+		//하얗게 불태웟다 도저히 무리다
+		//param.put("return_date", memberinfo.getReturnDate());
+		PaymentIOService.getInstance().updatePaymentIO(param);
+		pTabel.loadData();
+		JOptionPane.showMessageDialog(null, "반납되었습니다");
+		pBookinfo.clear();
+		pMemberDetail.clear();
 	}
+	
+	
+	protected void pBookinfoPBCodeTfBCodeMousePressed(MouseEvent e) {
+		//반납파트에서 눌럿을때 검색
+		BookSearchViewFrame bsv = new BookSearchViewFrame();
+		Map<String, Object> param = new HashMap<>();
+		param.put("isDel", false);
+		param.put("isLending", true);
+		bsv.setTableDate(param);
+		bsv.setVisible(true);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
