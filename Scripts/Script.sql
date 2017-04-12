91b0d03bb8ec4f861f1afcd7f46935b2ee84f2a8
inyoung <inyoung.estrella6y@naver.com>
2017-04-12 10:26:10 +0900

    파일 어디 갔다냐...

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -401,6 +401,54 @@ end $$
 delimiter ;
 
 
+
+
+
+
+-- 우편번호
+create table if not exists post(
+	zipcode	char(5)	null,
+	sido	varchar(20)	null,
+	sigungu	varchar(20) null,
+	doro	varchar(20) null,
+	building1 int(5) null,
+	building2 int(5) null
+);
+
+LOAD data LOCAL INFILE 'D:/workspace_mybatis/PostZipFinder/DataFiles/대구광역시.txt' 
+INTO table  post   
+character set 'euckr'  
+fields TERMINATED by '|' 
+IGNORE 1 lines 
+(@zipcode, @sido, @d, @sigungu , @d, @d, @d, @d, @doro, @d, @d, @building1, @building2, @d, @d, @d, @d, @d, @d ,@d, @d, @d, @d, @d, @d, @d) 
+set zipcode=@zipcode, sido=@sido, sigungu=@sigungu, doro=@doro, building1=@building1, building2=@building2;
+
+CREATE INDEX idx_post_sido On post(sido);
+CREATE INDEX idx_post_doro ON post(doro);
+
+select count(*) from post where sido = '대구광역시';
+select * from post where sido='대구광역시';
+
+CREATE INDEX idx_post_sido On post(sido);
+CREATE INDEX idx_post_doro ON post(doro);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
 
